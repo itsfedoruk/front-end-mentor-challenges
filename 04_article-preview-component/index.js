a11fcb508527d7d1c8703a02ca88216aa2e01fda
Maria Fedoruk <github@fedoruk.eu>
2021-01-10 18:04:37 +0100

    fix: add aria-controls and label to improve accessibility on the share button and box

--- 04_article-preview-component/index.js
+++ 04_article-preview-component/index.js
@@ -5,7 +5,19 @@ shareButton.addEventListener("click", () => {
     const authorBlock = document.querySelector(".author");
 
     shareBox.classList.toggle("share-box--hidden");
+
+    authorBlock.classList.toggle("author--hidden");
+
     shareButton.classList.toggle("btn__share-icon--open");
     shareButton.classList.toggle("btn__share-icon--static");
-    authorBlock.classList.toggle("author--hidden");
+
+   let ariaAttrButton = shareButton.getAttribute("aria-expanded");
+
+   if (ariaAttrButton == "false") {
+       shareButton.setAttribute("aria-expanded", "true");
+       shareButton.setAttribute("aria-label", "Close share options");
+   } else {
+       shareButton.setAttribute("aria-expanded", "false");
+       shareButton.setAttribute("aria-label", "Open share options");
+   }
 });
